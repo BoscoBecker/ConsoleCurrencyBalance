918a8928497d8a60cea5958650e423a11e123bc4
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-01-22 20:44:58 -0300

    Adicionar arquivos de projeto.

---
+++ Entidade/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCurrencyBalance.Entidade
+{
+    internal class Account
+    {
+        public int NumberAccount { get; set; }
+        public String NameAccount { get; set; }
+        public double Saldo { get; set; }
+
+        public void Depositar(double value)
+        {
+            Saldo += value;
+        }
+        public void Sacar(double value){
+            Saldo -= value;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + NumberAccount + " " +
+                   "Titular: " + NameAccount +" "+
+                   "Saldo " + "$" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleCurrencyBalance.Entidade;
+using System.Globalization;
+
+Console.WriteLine("----------------------- BANCO JB ------------------- ");
+Console.WriteLine("----------------------  Welcome  ------------------- ");
+
+Account CurrentBalance = new Account();
+Console.Write("Informe o número da conta :");
+CurrentBalance.NumberAccount = int.Parse(Console.ReadLine());
+Console.Write("Entre com o titular da conta: ");
+CurrentBalance.NameAccount = Console.ReadLine();
+Console.WriteLine("Haverá depósito Inicial (S/N) ? ");
+char Question1 = Console.ReadKey().KeyChar;
+
+if (Question1 == 'S')
+{
+    Console.WriteLine("Entre o valor de depósito inicial: ");
+    CurrentBalance.Depositar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+    Console.WriteLine("Dados da conta: " + CurrentBalance);
+}
+
+Console.Write("Entre um valor para depósito:");
+CurrentBalance.Depositar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+Console.WriteLine("Dados atualizados: " + CurrentBalance);
+Console.Write("Entre com um valor para saque:");
+CurrentBalance.Sacar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+Console.WriteLine("Dados da conta atualizados: " + CurrentBalance);
